ef859d2f82bbe520cccad3cf96d0d15e29ed5094
Pablito <PavelVolkovPro@yandex.ru>
2022-10-12 21:07:51 +0300

    Main block

--- code/Program.cs
+++ code/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] StartArr = {"lol", "news", "kek", "abcd", "1", "100000"};
+int len = StartArr.Length;
+string[] FinishArr = new string[len];
+
+int j = 0;
+
+for(int i = 0; i < StartArr.Length; i++){
+    if(StartArr[i].Length <= 3){
+        FinishArr[j] = StartArr[i];
+        j++;
+    }
+}
+
+for(int i = 0; i < FinishArr.Length; i++){
+    Console.Write($"{FinishArr[i]} ");
+}
